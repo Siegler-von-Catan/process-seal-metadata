a75e7b73c23a03ac9920f6b530f5415840ced858
leogeier <leonard.geier@hotmail.com>
2021-01-22 18:05:15 +0100

    fix license

--- init_script.sql
+++ init_script.sql
@@ -1,3 +1,23 @@
+/* 
+ * ProcessSeaMetadata - Summarize relevant seal metadata in a useable format
+ * Copyright (C) 2021
+ * Joana Bergsiek, Leonard Geier, Lisa Ihde,
+ * Tobias Markus, Dominik Meier, Paul Methfessel
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 CREATE TABLE IF NOT EXISTS seal (
   id INTEGER PRIMARY KEY,
   family TEXT,
--- process_metadata.py
+++ process_metadata.py
@@ -1,5 +1,23 @@
 #!/usr/bin/env python3
 
+# ProcessSeaMetadata - Summarize relevant seal metadata in a useable format
+# Copyright (C) 2021
+# Joana Bergsiek, Leonard Geier, Lisa Ihde,
+# Tobias Markus, Dominik Meier, Paul Methfessel
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 from argparse import ArgumentParser
 from pathlib import Path
 import xml.etree.ElementTree as ET
